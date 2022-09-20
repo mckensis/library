be43c51afef7369e1532f33a480aa0cf81c4d806
Aidan <adn.mck@gmail.com>
2022-09-20 03:19:23 +0100

    updated interface, add book needs programmed

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let bookLibrary = [];
-const pageLibrary = document.querySelector(".mainLibrary");
-const addBookButton = document.querySelector(".addBook");
+const pageLibrary = document.querySelector(".libraryDisplay");
+const addBookButton = document.querySelector(".addBookButton");
+const cancelButton = document.querySelector(".cancelButton");
+const saveButton = document.querySelector(".saveButton");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -12,18 +14,44 @@ function Book(title, author, pages, read) {
     }
 }
 
-addBookButton.addEventListener("click", newBook);
+addBookButton.addEventListener("click", addNewBook);
+cancelButton.addEventListener("click", removeOverlay);
+saveButton.addEventListener("click", saveBook);
 
-function newBook() {
-    console.log("new book");
+function loadOverlay() {
+    const overlay = document.createElement("div");
+    overlay.classList.add("overlay");
+    document.body.appendChild(overlay);
 }
 
-// example:
-let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
-let booky = new Book("test book", "test author", 666, "read");
-bookLibrary.push(theHobbit);
-bookLibrary.push(booky);
+function removeOverlay(e) {
+    e.preventDefault();
+    const overlay = document.querySelector(".overlay");
+    const popup = document.querySelector(".newBookPopup");
+    popup.style.visibility = "hidden";
+    document.body.removeChild(overlay);
+}
 
+function saveBook(e) {
+    e.preventDefault();
+    console.log("saved");
+    removeOverlay(e);
+}
+
+function addNewBook(e) {
+    e.preventDefault();
+    const popup = document.querySelector(".newBookPopup");
+    popup.style.visibility = "visible";
+    loadOverlay();
+}
+
+for (let i = 0; i < 20; i++) {
+    let book = new Book("The Lord of the Rings, The Fellowship of the Ring", "Aidan Mckenzie djejejej", 9003, true);
+    bookLibrary.push(book);
+}
+
+let abook = new Book("anotha test", "adn", 666, false);
+bookLibrary.push(abook);
 
 for (const book of bookLibrary) {
     let div = document.createElement("div");
@@ -31,15 +59,20 @@ for (const book of bookLibrary) {
     let author = document.createElement("p");
     let pages = document.createElement("p");
     let read = document.createElement("p");
-
+    
     title.textContent = book.title;
     author.textContent = book.author;
-    pages.textContent = book.pages;
-    read.textContent = book.read;
-    div.classList.add("book");
+    pages.textContent = `${book.pages} pages`;
+    div.classList.add("bookCard");    
     div.appendChild(title);
     div.appendChild(author);
     div.appendChild(pages);
-    div.appendChild(read);
+
+    if (book.read) {
+        read.textContent = "read";
+        read.classList.add("readBook");
+        div.appendChild(read);
+    }
+
     pageLibrary.appendChild(div);
 }
