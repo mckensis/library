8f89e831121a35695b22c05846ccb8a181203daf
Aidan <adn.mck@gmail.com>
2022-09-20 16:50:37 +0100

    functioning as required. to-do: styling update

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let bookLibrary = [];
-const pageLibrary = document.querySelector(".libraryDisplay");
+const libraryDisplay = document.querySelector(".libraryDisplay");
 const addBookButton = document.querySelector(".addBookButton");
 const cancelButton = document.querySelector(".cancelButton");
 const saveButton = document.querySelector(".saveButton");
+
+let bookLibrary = [];
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -14,11 +15,7 @@ function Book(title, author, pages, read) {
     }
 }
 
-addBookButton.addEventListener("click", addNewBook);
-cancelButton.addEventListener("click", removeOverlay);
-saveButton.addEventListener("click", saveBook);
-
-function loadOverlay() {
+function addOverlay() {
     const overlay = document.createElement("div");
     overlay.classList.add("overlay");
     document.body.appendChild(overlay);
@@ -32,47 +29,159 @@ function removeOverlay(e) {
     document.body.removeChild(overlay);
 }
 
-function saveBook(e) {
-    e.preventDefault();
-    console.log("saved");
-    removeOverlay(e);
-}
+function checkBookValidity() {
 
-function addNewBook(e) {
-    e.preventDefault();
-    const popup = document.querySelector(".newBookPopup");
-    popup.style.visibility = "visible";
-    loadOverlay();
-}
+    const bookTitle = document.querySelector("#bookTitle");
+    const bookAuthor = document.querySelector("#bookAuthor");
+    const bookPages = document.querySelector("#bookPages");
 
-for (let i = 0; i < 20; i++) {
-    let book = new Book("The Lord of the Rings, The Fellowship of the Ring", "Aidan Mckenzie djejejej", 9003, true);
-    bookLibrary.push(book);
-}
-
-let abook = new Book("anotha test", "adn", 666, false);
-bookLibrary.push(abook);
-
-for (const book of bookLibrary) {
-    let div = document.createElement("div");
-    let title = document.createElement("h2");
-    let author = document.createElement("p");
-    let pages = document.createElement("p");
-    let read = document.createElement("p");
-    
-    title.textContent = book.title;
-    author.textContent = book.author;
-    pages.textContent = `${book.pages} pages`;
-    div.classList.add("bookCard");    
-    div.appendChild(title);
-    div.appendChild(author);
-    div.appendChild(pages);
-
-    if (book.read) {
-        read.textContent = "read";
-        read.classList.add("readBook");
-        div.appendChild(read);
+    if (bookTitle.checkValidity() && bookAuthor.checkValidity() && bookPages.checkValidity()) {
+        return true;
     }
 
-    pageLibrary.appendChild(div);
+    return false;
 }
+
+function saveBook(e) {
+
+    if (!checkBookValidity()) {
+        return;
+    }
+
+    const book = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.checked);
+    bookLibrary.push(book);
+    removeOverlay(e);
+    updateLibrary();
+}
+
+function clearForm() {
+    const title = document.querySelector("#bookTitle");
+    const author = document.querySelector("#bookAuthor");
+    const pages = document.querySelector("#bookPages");
+    const read = document.querySelector("#bookRead");
+
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+    read.checked = false;
+
+    title.focus();
+}
+
+function addNewBookButton(e) {
+    
+    e.preventDefault();
+    addOverlay();
+
+    const popup = document.querySelector(".newBookPopup");
+    popup.style.visibility = "visible";
+
+    clearForm();
+
+    cancelButton.addEventListener("click", removeOverlay);
+    saveButton.addEventListener("click", saveBook);
+}
+
+function removeBook(e) {
+    const book = e.target.id;
+    bookLibrary.splice(book, 1);
+    updateLibrary();
+}
+
+function updateLibrary() {
+    
+    while(libraryDisplay.childNodes.length > 2) {
+        libraryDisplay.removeChild(libraryDisplay.lastChild);
+    }
+
+    for (const book of bookLibrary) {
+        let div = document.createElement("div");
+        let title = document.createElement("h2");
+        let author = document.createElement("p");
+        let pages = document.createElement("p");
+        let readP = document.createElement("p");
+        let buttonDiv = document.createElement("div");
+        let readIcon = document.createElement("button");
+        let delIcon = document.createElement("button");
+        
+        readIcon.classList.add("readButton");
+        delIcon.classList.add("delButton");
+
+        readIcon.id = bookLibrary.indexOf(book);
+        delIcon.id = bookLibrary.indexOf(book);
+
+        title.textContent = book.title;
+        author.textContent = book.author;
+        pages.textContent = book.pages;
+
+        if (book.pages > 1) {
+            pages.textContent += " pages";
+        } else {
+            pages.textContent += " page";
+        }
+        div.classList.add("bookCard");    
+        div.appendChild(title);
+        div.appendChild(author);
+        div.appendChild(pages);
+        
+        if (book.read) {
+            readP.textContent = "read";
+            readP.classList.add("readBook");
+            readIcon.classList.add("toggled");
+        }
+
+        readP.id = bookLibrary.indexOf(book);
+        div.appendChild(readP);
+
+        buttonDiv.appendChild(readIcon);
+        buttonDiv.appendChild(delIcon);
+
+        buttonDiv.classList.add("buttonDiv");
+        div.appendChild(buttonDiv);
+        delIcon.addEventListener("click", removeBook);
+        readIcon.addEventListener("click", toggleRead);
+        libraryDisplay.appendChild(div);
+    }
+}
+
+function toggleRead(e) {
+    
+    e.preventDefault();
+    const icon = e.target;
+    const read = e.target.parentNode.previousElementSibling;
+    
+    const book = bookLibrary[icon.id];
+    
+    if (read.classList.contains("readBook")) {
+        book.read = false;
+        read.classList.remove("readBook");
+        icon.classList.remove("toggled");
+        read.textContent = '';
+    } else {
+        book.read = true;
+        read.classList.add("readBook");
+        icon.classList.add("toggled");
+        read.textContent = "read";
+    }
+}
+
+// Populate the library with some default books.
+function defaultBooks() {
+    
+    const murakami = new Book("The Wind Up Bird Chronicle", "Haruki Murakami", 607, true);
+    const trainspotting = new Book("Trainspotting", "Irvine Welsh", 344, false);
+    const marabou = new Book("Marabou Stork Nightmares", "Irvine Welsh", 264, true);
+    const pollen = new Book("Pollen", "Jeff Noon", 327, false);
+    const gone = new Book("Gone Girl", "Gillian Flynn", 432, false);
+
+    bookLibrary.push(murakami);
+    bookLibrary.push(trainspotting);
+    bookLibrary.push(marabou);
+    bookLibrary.push(pollen);
+    bookLibrary.push(gone);
+
+    updateLibrary();
+}
+defaultBooks();
+
+addBookButton.addEventListener("click", addNewBookButton);
